8da06d625d6d102a8a6e83e817d02bba5b641d72
Scott Morse <scottmorsedev@gmail.com>
2018-10-27 21:43:53 -0500

    More SQL ready-to-go funcs

--- modules/sqlCom.js
+++ modules/sqlCom.js
@@ -38,17 +38,122 @@ function selectDb(dbName){
 function createTable(tbName,argString,createPrimaryKey){
     argString = " " + argString || ""
     createPrimaryKey = createPrimaryKey || false
-    if(createPrimaryKey){
-        if(argString){
-            argString = '(key INT AUTO_INCREMENT PRIMARY KEY, ' + argString.slice(1,argString.length)
-        }
-        else{
-            argString = '(key INT AUTO_INCREMENT PRIMARY KEY)'
-        }
-    }
+
     dbCon.query('CREATE TABLE ' + tbName + argString,(err,result) => {
         if (err) console.log('Did NOT create table ' + tbName)
+        else
+        {
         console.log("Table " + tbName + " created")
+        if(createPrimaryKey && !argString.includes('AUTO_INCREMENT PRIMARY KEY')){
+            dbCon.query("ALTER TABLE " + tbName + " ADD COLUMN key INT AUTO_INCREMENT PRIMARY KEY",(err,result) => {
+                if (err) throw err
+            })
+        }
+    }
+    })
+}
+
+function insertData(tbName,columnArr,valueDArr){
+    const comm = "INSERT INTO " + tbName + " (" + columnArr.join(", ") + ") VALUES ?"
+    dbCon.query(comm,valueArr,(err,result) => {
+        if (err) throw err;
+        console.log(result.affectedRows + " record(s) inserted to " + tbName)
+        return result
+    })
+}
+
+function updateData(tbName,value,valueColumn,filterValue,filterColumn){
+    const comm = "UPDATE " + tbName + " SET " + valueColumn + " = " + valueCanyon + " WHERE " + filterColumn + " = " + filterValue;
+    dbCon.query(comm,valueArr,(err,result) => {
+        if (err) throw err
+        console.log("Record(s) for " + filterColumn + " " + filterValue + " updated to " + value)
+        return result
+    })
+}
+
+//result is an array of objects of all records
+//fields is an array of objects about each column
+function selectTable(tbName,columnArr,limit,limitOffset){
+    let comm
+    if(!columnArr)
+    {
+        comm = "SELECT * FROM " + tbName
+    }
+    else
+    {
+        comm = "SELECT " + columnArr.join(", ") + "FROM " + tbName
+    }
+    if(limit){
+        comm += " LIMIT " + limit
+    }
+    if(limit && limitOffset){
+        comm += " OFFSET " + limitOffset
+    }
+    dbCon.query(comm,(err,result,fields) => {
+        if (err) throw err
+        return result
+    })
+}
+
+function selectTableFiltered(tbName,filterColumn,filter,columnArr,limit,limitOffset){
+    let comm
+    if(!columnArr)
+    {
+        comm = "SELECT * FROM " + tbName + " WHERE " + filterColumn + " = '" + filter + "'"
+    }
+    else
+    {
+        comm = "SELECT " + columnArr.join(", ") + " FROM " + tbName + " WHERE " + filterColumn + " = '" + filter + "'"
+    }
+    if(limit){
+        comm += " LIMIT " + limit
+    }
+    if(limit && limitOffset){
+        comm += " OFFSET " + limitOffset
+    }
+    dbCon.query(comm,(err,result,fields) => {
+        if (err) throw err
+        return result
+    })
+}
+
+function selectSortedTable(tbName,sortColumn,columnArr,desc,limit,limitOffset){
+    let comm
+    if(!columnArr)
+    {
+        comm = "SELECT * FROM " + tbName + " ORDER BY " + sortColumn
+    }
+    else
+    {
+        comm = "SELECT " + columnArr.join(", ") + " FROM " + tbName + " ORDER BY " + sortColumn
+    }
+    if(desc){
+        comm += " DESC"
+    }
+    if(limit){
+        comm += " LIMIT " + limit
+    }
+    if(limit && limitOffset){
+        comm += " OFFSET " + limitOffset
+    }
+    dbCon.query(comm,(err,result,fields) => {
+        if (err) throw err
+        return result
+    })
+}
+
+function deleteRecords(tbName,filterColumn,filter){
+    const comm = "DELETE FROM " + tbName + " WHERE " + filterColumn + " = '" + filter + "'"
+    dbCon.query(comm,(err,result) => {
+        if (err) throw err
+        return result
+    })
+}
+
+function deleteTable(tbName){
+    dbCon.query("DROP TABLE IF EXISTS " + tbName,(err,result) => {
+        if (err) throw err
+        console.log("Table " + tbName + " deleted")
     })
 }
 
